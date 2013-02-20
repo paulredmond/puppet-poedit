0e76c0a95591e45a5ffe415e3ed11e7a7de004e8
Paul Redmond <paulrredmond@gmail.com>
2013-02-20 16:55:40 -0700

    Correct source :\

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 class poedit {
 
   package { 'Poedit':
-    source   => 'http://downloads.sourceforge.net/project/poedit/poedit/1.5/poedit-1.5.5.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fpoedit%2Ffiles%2Fpoedit%2F1.5%2F&ts=1361404033&use_mirror=voxel',
+    source   => 'http://iweb.dl.sourceforge.net/project/poedit/poedit/1.5/poedit-1.5.5.dmg',
     provider => 'appdmg'
   }
 }
