c46e02982d7526668816afaa7392e5e2ca9c4f96
Paul Redmond <paulrredmond@gmail.com>
2013-02-20 17:03:51 -0700

    Try this again...

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,8 @@
 class poedit {
 
   package { 'Poedit':
+    provider => pkgdmg,
+    ensure   => installed,
     source   => 'http://iweb.dl.sourceforge.net/project/poedit/poedit/1.5/poedit-1.5.5.dmg',
-    provider => 'appdmg'
   }
 }
